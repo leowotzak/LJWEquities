4e5afdf3b3927a9b54c6a6fd5ea7c1d44d649a3b
Leo Wotzak <leojwotzak@gmail.com>
2021-08-24 17:53:09 -0500

    added logging

--- autoGIS/main.py
+++ autoGIS/main.py
@@ -8,5 +8,9 @@ from arcgis.gis import GIS
 from arcgis.geocoding import get_geocoders, batch_geocode, geocode
 from arcgis.geoenrichment import enrich
 
+# * Logging
+import logging
+logging.basicConfig(filename='main.log', encoding='utf-8', level=logging.INFO)
+
 if __name__ == "__main__":
 
