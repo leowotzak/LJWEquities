be0680b0ff0ab71692fe4327f04cf828ab4e7200
Leo Wotzak <leojwotzak@gmail.com>
2021-08-24 17:52:43 -0500

    initialized file as script

--- autoGIS/main.py
+++ autoGIS/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import argparse
 import pandas as pd
@@ -5,3 +7,6 @@ import pandas as pd
 from arcgis.gis import GIS
 from arcgis.geocoding import get_geocoders, batch_geocode, geocode
 from arcgis.geoenrichment import enrich
+
+if __name__ == "__main__":
+
