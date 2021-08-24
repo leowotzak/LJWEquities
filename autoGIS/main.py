99021cfb88da6f1027d2a4e110367e9b6371375e
Leo Wotzak <leojwotzak@gmail.com>
2021-08-24 17:53:37 -0500

    loaded environment into script (dotenv)

--- autoGIS/main.py
+++ autoGIS/main.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import pandas as pd
 
+from dotenv import load_dotenv
 from arcgis.gis import GIS
 from arcgis.geocoding import get_geocoders, batch_geocode, geocode
 from arcgis.geoenrichment import enrich
@@ -13,4 +14,6 @@ import logging
 logging.basicConfig(filename='main.log', encoding='utf-8', level=logging.INFO)
 
 if __name__ == "__main__":
+
+    load_dotenv()
 
