9ec968cb7306c2fb9bb77f81c57779a7aa6db113
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 19:57:17 -0500

    import sql alchemy & modules

--- LJWEquities/ljwtrader/sql.py
+++ LJWEquities/ljwtrader/sql.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from dotenv import load_dotenv
 
+from sqlalchemy import create_engine, Table, Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker
 if __name__ == '__main__':
 
     load_dotenv()
