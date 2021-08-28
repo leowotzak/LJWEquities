bea0f0082bfe1c3c7170ddb84b09c01196bda6bd
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 19:58:13 -0500

    Initialized sql settings

--- LJWEquities/ljwtrader/sql.py
+++ LJWEquities/ljwtrader/sql.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine, Table, Column, Integer, String, Float, Dat
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_engine('sqlite+pysqlite:///:memory:', echo=True, future=True)
+Base = declarative_base()
+Session = sessionmaker(engine)
+
 if __name__ == '__main__':
 
     load_dotenv()
