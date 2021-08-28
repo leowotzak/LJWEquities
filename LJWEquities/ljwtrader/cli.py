e1c311bfaef81b3879b8f73a3b841a26c4315fd8
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 20:52:33 -0500

    Added argument parsing

--- LJWEquities/ljwtrader/cli.py
+++ LJWEquities/ljwtrader/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 
 # * File that acts as input so that I dont have to edit the base files anymore
 
@@ -6,19 +7,34 @@ parser = argparse.ArgumentParser(description="LJWE quantitative trading system")
 
 
 # * First day of analysis
-parser.add_argument('start'),
+parser.add_argument(
+    'start',
+    type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+    nargs=1,
+    ),
 
 # * Last day of analysis
-parser.add_argument('end'),
+parser.add_argument(
+    'end',
+    type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+    nargs=1,
+    ),
 
 # * Frequency of the data (i.e. 5min 5day etc...)
-parser.add_argument('frequency'),
+parser.add_argument(
+    'frequency',
+    type=str,
+    nargs=1,
+    choices=['1min', '5min', '15min', '30min', '60min']
+    ),
 
 # * Vendor of data 
 # ? Is this necessary
 # ? Or should be be determined later?
-parser.add_argument('vendor')
-
-# * Action to perform, starts backtest
-parser.add_argument('--backtest')
+parser.add_argument(
+    'vendor',
+    type=str,
+    nargs=1,
+    choices=['av']
+    )
 
