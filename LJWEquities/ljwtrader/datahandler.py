d4fa7521fb022a95d26404860f7fb246cd6d6ca2
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 20:09:22 -0500

    made attributes private (_)

--- LJWEquities/ljwtrader/datahandler.py
+++ LJWEquities/ljwtrader/datahandler.py
@@ -6,7 +6,7 @@ class DataHandler:
     """Object that handles all data access for other system components"""
     def __init__(self, queue_, start_date, end_date, frequency, vendor):
         self._queue = queue_
-        self.start_date = start_date
-        self.end_date = end_date
-        self.frequency = frequency
-        self.vendor = vendor
+        self._start_date = start_date
+        self._end_date = end_date
+        self._frequency = frequency
+        self._vendor = vendor
