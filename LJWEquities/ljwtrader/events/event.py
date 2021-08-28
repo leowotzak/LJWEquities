8ca075f9a2b233c73afa2d380a421b8b6124989f
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 19:30:51 -0500

    Added event __str__ & __repr__

--- LJWEquities/ljwtrader/events/event.py
+++ LJWEquities/ljwtrader/events/event.py
@@ -6,3 +6,11 @@ class EventBaseClass:
     def __init__(self, event_type, datetime, symbol):
         self.event_type = event_type
         self.datetime = datetime
+
+    def __str__(self):
+        return f'{self.event_type} -- {self.datetime}'
+
+    def __repr__(self):
+        repr = ' -- '.join([
+            f'{key}: {value}' for key, value in self.__dict__.items()
+        ])
