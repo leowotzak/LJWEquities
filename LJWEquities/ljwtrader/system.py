b936c9ad5104e61d98fb2672000cfecdf8e0f99c
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 19:19:04 -0500

    Added TradingSystem class as primary system object

--- LJWEquities/ljwtrader/system.py
+++ LJWEquities/ljwtrader/system.py
@@ -4,3 +4,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class TradingSystem:
+    """
+    Trading system that serves to govern the entire trading logic process which it does by reading the event loop,
+    pushing data from the data handler, and calculating trading signals.
+    """
