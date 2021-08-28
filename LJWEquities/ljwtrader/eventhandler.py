579006412f07b74b40411b7ed6fbb9658dd33925
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 19:49:17 -0500

    Added market event and its handler

--- LJWEquities/ljwtrader/eventhandler.py
+++ LJWEquities/ljwtrader/eventhandler.py
@@ -4,3 +4,13 @@ logger = logging.getLogger(__name__)
 
 class EventHandler:
     """Object that serves to route events to the proper system component based on their type"""
+
+    def __init__(self, queue):
+        self._queue = queue
+    
+    def _handle_market(event):
+        pass
+    
+    EVENT_MAP = {
+        'MARKET': _handle_market
+    }
