4e08c01f57a22dbd48917bd14c31781e7f652c42
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 19:44:14 -0500

    Initiated input attributes (from cli.py)

--- LJWEquities/ljwtrader/system.py
+++ LJWEquities/ljwtrader/system.py
@@ -6,6 +6,17 @@ logger = logging.getLogger(__name__)
 
 class TradingSystem:
     """
-    Trading system that serves to govern the entire trading logic process which it does by reading the event loop,
-    pushing data from the data handler, and calculating trading signals.
+    Object that serves to govern the entire trading logic process
+
+    All user input should be applied in this class, which acts as a unifier between all of the system components.
+    The TradingSystem pushes data from the Data handler to the Event handler.
     """
+    def __init__(self, start_date, end_date, frequency, vendor):    
+
+        self.start_date = start_date
+        self.end_date = end_date
+        self.frequency = frequency
+        self.vendor = vendor
+        self._queue = Queue()
+
+        logger.info(f"Start Date: {self.start_date}, End Date: {self.end_date}, Frequency: {self.frequency}, Vendor {self.vendor}")
