ed59186f9e105182c66823425c6e4e0d5ad8f07c
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 19:30:24 -0500

    Initialized events with a type, time, and symbol

--- LJWEquities/ljwtrader/events/event.py
+++ LJWEquities/ljwtrader/events/event.py
@@ -3,3 +3,6 @@ class EventBaseClass:
     Event is base class providing an interface for all subsequent (inherited) events, that will trigger further
     events in the trading infrastructure.
     """
+    def __init__(self, event_type, datetime, symbol):
+        self.event_type = event_type
+        self.datetime = datetime
