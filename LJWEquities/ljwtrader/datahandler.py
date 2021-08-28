a6d7e1f50eb76f9b0ab92ffc21211556901b600f
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 20:07:38 -0500

    initialized datahandler object

--- LJWEquities/ljwtrader/datahandler.py
+++ LJWEquities/ljwtrader/datahandler.py
@@ -4,3 +4,9 @@ logger = logging.getLogger(__name__)
 
 class DataHandler:
     """Object that handles all data access for other system components"""
+    def __init__(self, queue_, start_date, end_date, frequency, vendor):
+        self._queue = queue_
+        self.start_date = start_date
+        self.end_date = end_date
+        self.frequency = frequency
+        self.vendor = vendor
