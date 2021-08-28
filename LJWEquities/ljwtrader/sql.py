2069100a06c625c255f45ef5cc2fb7596dd94051
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 19:57:45 -0500

    created sql engine

--- LJWEquities/ljwtrader/sql.py
+++ LJWEquities/ljwtrader/sql.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 from sqlalchemy import create_engine, Table, Column, Integer, String, Float, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine('sqlite+pysqlite:///:memory:', echo=True, future=True)
 if __name__ == '__main__':
 
     load_dotenv()
