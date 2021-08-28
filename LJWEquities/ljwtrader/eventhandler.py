48d860298dd97d681f6a398d8ac0856c424d2b6f
Leo Wotzak <leojwotzak@gmail.com>
2021-08-27 20:53:38 -0500

    Added datahandler to eventhandler

--- LJWEquities/ljwtrader/eventhandler.py
+++ LJWEquities/ljwtrader/eventhandler.py
@@ -5,12 +5,13 @@ logger = logging.getLogger(__name__)
 class EventHandler:
     """Object that serves to route events to the proper system component based on their type"""
 
-    def __init__(self, queue):
+    def __init__(self, queue, data_handler):
         self._queue = queue
+        self._data_handler = data_handler
     
-    def _handle_market(event):
+    def _handle_market(self, event):
         pass
-    
+
     EVENT_MAP = {
         'MARKET': _handle_market
     }
