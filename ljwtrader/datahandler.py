323e0b0fd0344cd011d8b37c8f778df6ca1a40ae
Leo Wotzak <leojwotzak@gmail.com>
2021-08-30 19:06:03 -0500

    imported event_handler function

--- ljwtrader/datahandler.py
+++ ljwtrader/datahandler.py
@@ -9,12 +9,14 @@ logger = logging.getLogger(__name__)
 
 class DataHandler:
     """Object that handles all data access for other system components"""
-    def __init__(self, queue_, start_date, end_date, frequency, vendor):
+    def __init__(self, queue_, start_date, end_date, frequency, vendor, process_events_func):
         self._queue = queue_
         self._start_date = start_date
         self._end_date = end_date
         self._frequency = frequency
         self._vendor = vendor
+        self._process_events_func = process_events_func
+
         self.data = (
             (index, row)
             for index, row in pd.read_sql('SELECT * FROM daily_bar_data',
@@ -23,4 +25,5 @@ class DataHandler:
                                           .sort_index()
                                           .iterrows())
 
-        self.data = ((index, row) for index, row in pd.read_sql('SELECT * FROM daily_bar_data', sqlite3.connect('app.db'), index_col='timestamp').iterrows())
+            self._process_events_func()
+
