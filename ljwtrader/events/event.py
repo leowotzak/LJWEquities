9d48396d6c8d2a2f6b30e2e50a30ef3ac002287a
Leo Wotzak <leojwotzak@gmail.com>
2021-08-30 19:04:15 -0500

    Added ticker as an attribute

--- ljwtrader/events/event.py
+++ ljwtrader/events/event.py
@@ -3,12 +3,13 @@ class EventBaseClass:
     Event is base class providing an interface for all subsequent (inherited) events, that will trigger further
     events in the trading infrastructure.
     """
-    def __init__(self, event_type, datetime):
+    def __init__(self, event_type, ticker, datetime):
         self.event_type = event_type
+        self.ticker = ticker
         self.datetime = datetime
 
     def __str__(self):
-        return f'{self.event_type} -- {self.datetime}'
+        return f'  --  '.join([self.ticker, self.event_type, self.datetime])
 
     def __repr__(self):
         repr = ' -- '.join([
--- ljwtrader/events/marketevent.py
+++ ljwtrader/events/marketevent.py
@@ -2,5 +2,5 @@ from .event import EventBaseClass
 
 class MarketEvent(EventBaseClass):
     """Created in response to price changes in underlying assets"""
-    def __init__(self, time):
-        super().__init__('MARKET', time)
+    def __init__(self, ticker, time):
+        super().__init__('MARKET', ticker, time)
