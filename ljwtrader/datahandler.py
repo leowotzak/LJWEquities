6920a07c66381eda04517d468616bf12fc1fa9dc
Leo Wotzak <leojwotzak@gmail.com>
2021-08-31 12:26:21 -0500

    created latest symbol data & getter

--- ljwtrader/datahandler.py
+++ ljwtrader/datahandler.py
@@ -35,8 +35,7 @@ class DataHandler:
         self._vendor = vendor
         self._contine_backtest = False
         self._process_events_func = process_events_func
-        self.data = ( tup for tup in pd.read_sql("SELECT * FROM symbols JOIN daily_bar_data ON symbols.symbol_id=daily_bar_data.symbol_id WHERE symbols.ticker IN ('%s')" % "', '".join(self._symbols),
-                                          sqlite3.connect('app.db'),
+        self.latest_symbol_data = {sym: {} for sym in self._symbols}
                                           index_col='timestamp').sort_index().groupby(level=0) )
         self.latest_symbol_data = { sym: {} for sym in self._symbols }
 
@@ -51,14 +50,15 @@ class DataHandler:
         else:
             for bar in map(convert_bar, row.iterrows()):
                 self._queue.put(MarketEvent(bar['ticker'], bar['timestamp']))
-                self.latest_symbol_data[bar['ticker']][index]= bar
+                self.latest_symbol_data[bar['ticker']][index] = bar
 
-    def get_latest_symbol_data(self, ticker: str, category: str, num_days: int) -> np.ndarray:
+    def _get_latest_symbol_data(self, ticker: str, category: str,
+                                num_days: int) -> np.ndarray:
         """Retrieves the bars for a given ticker's category over the # of specified days"""
 
         latest_data = self.latest_symbol_data[ticker].copy()
         try:
-            arr = [ latest_data.popitem()[1][category] for _ in range(num_days) ]
+            arr = [latest_data.popitem()[1][category] for _ in range(num_days)]
         except KeyError as e:
             arr = []
         return np.array(arr)
@@ -70,4 +70,3 @@ class DataHandler:
         while self._contine_backtest:
             self._get_next_bar()
             self._process_events_func()
-
