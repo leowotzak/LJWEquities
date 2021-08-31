7f38173e9278cf26c820000aed85356a8316ed3a
Leo Wotzak <leojwotzak@gmail.com>
2021-08-31 16:22:52 -0500

    added type hints

--- ljwtrader/strategy/position.py
+++ ljwtrader/strategy/position.py
@@ -1,20 +1,22 @@
 from abc import ABCMeta, abstractmethod
-from typing import List, Callable
+from typing import List, Callable, Any
 import numpy as np
 from .indicator import HighFunc
+from ljwtrader.datahandler import DataHandler
+
 import logging
 logger = logging.getLogger(__name__)
 
 
 class Position(metaclass=ABCMeta):
 
-    indicator: Indicator
+    indicator: Any
     condition: Callable[[Any], bool]
     value: Any
     conditions: List[Callable]
 
     @abstractmethod
-    def check(self, arr: np.ndarray) -> bool:
+    def check(self, data_handler: DataHandler) -> bool:
         """Called by strategy object on each tick"""
         raise NotImplementedError(
             """Position needs to have a condition check to know if it is active or not"""
@@ -22,7 +24,6 @@ class Position(metaclass=ABCMeta):
 
 
 class HighPosition(Position):
-    def __init__(self, indicator: Indicator, condition, value: Any):
-        self.indicator = indicator
+    def __init__(self, indicator: Indicator, ticker: str, N: int, condition, value: Any):
         self.condition = condition
         self.value = value
