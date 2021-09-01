b4cfb4e0e28bac36b53ff8032d9881dc524c3f1e
Leo Wotzak <leojwotzak@gmail.com>
2021-08-31 19:48:24 -0500

    started monitoring high_price

--- ljwtrader/datahandler.py
+++ ljwtrader/datahandler.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 def convert_bar(row):
     index, data = row   #* These are the fields the system watches
     output_dict = dict((k, v) for k, v in data.to_dict().items()
-                       if k in ['ticker', 'adj_close_price'])       
+                       if k in ['ticker', 'adj_close_price', 'high_price'])       
     output_dict['timestamp'] = index
     return output_dict
 
