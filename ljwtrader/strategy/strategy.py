8d71aa5bde7ecb9a2eeda5b0ae0e3e7d3eab0809
Leo Wotzak <leojwotzak@gmail.com>
2021-09-01 10:27:57 -0500

    combined position & strategy into single strategy

--- ljwtrader/strategy/strategy.py
+++ ljwtrader/strategy/strategy.py
@@ -1,11 +1,11 @@
 from abc import ABCMeta, abstractmethod
 import logging
 import operator
-from typing import List
+from typing import List, AnyStr
 
 import numpy as np
 from ljwtrader.datahandler import DataHandler
-from .position import Position
+from ljwtrader.strategy.indicator import XDayHigh
 
 logger = logging.getLogger(__name__)
 
@@ -26,14 +26,23 @@ class Strategy(metaclass=ABCMeta):
 
 
 class Strategy(Strategy):
+    def __init__(self, symbols: List[AnyStr], N: int, operator_, value: float,
                  data_handler: DataHandler):
 
-        self.conditional_positions = conditional_positions
+        self.symbols = symbols
+        self.N = N
+        self.operator = operator_
         self.data_handler = data_handler
+        self.value = value
+
+        self.conditional_positions = []
+        for symbol in self.symbols:
+            self.conditional_positions.append(XDayHigh(symbol, self.N, self.operator, self.value))
+
 
     def check_all(self):
         results = []
         for position in self.conditional_positions:
             calc_value = position(self.data_handler)
             results.append(calc_value)
-            logger.debug(f'Ticker: {position.ticker} Check: {calc_value}')
+            logger.debug(f'Check: {calc_value}')
