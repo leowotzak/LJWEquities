12d9fa7bf8e077fc478fceba3f7c51bf6e64103a
Leo Wotzak <leojwotzak@gmail.com>
2021-09-04 02:14:18 -0500

    added portfolio class description

---
+++ ljwtrader/portfolio.py
@@ -0,0 +1,8 @@
+class Portfolio:
+    """
+    Responsible for handling and implementing all portfolio logic for the trading system. 
+    At a high level, the portfolio receives signals from all of the strategies that constitute
+    it. The portfolio can then decide whether to follow through with these strategies, or it can 
+    ignore them in favor of some other over-arching portfolio strategy. The portfolio should also 
+    keep track of any rebalancing and issue orders to achieve certain targets.
+    """
