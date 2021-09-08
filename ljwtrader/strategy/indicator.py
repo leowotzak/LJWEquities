7d5e7db0307f40442afb9871eea7f5fa064911d3
Leo Wotzak <leojwotzak@gmail.com>
2021-09-07 19:27:40 -0500

    changed return type to lambda

--- ljwtrader/strategy/indicator.py
+++ ljwtrader/strategy/indicator.py
@@ -1,24 +1,26 @@
 import logging
-from typing import Any, Callable, NewType
+import operator
+from typing import Any, AnyStr, Callable, NewType
 
 import numpy as np
 from ljwtrader.datahandler import DataHandler
 
-Indicator = NewType('Indicator', Callable[[DataHandler], float])
+Indicator = NewType('Indicator', Callable[[DataHandler], bool])
 
 logger = logging.getLogger(__name__)
 
+# ! Issue with how this function is added to new objects 
+# ! To call the outer function,  no self attribute is needed for
+# ! But when you do not use self you cannot use the associated ticker
 
 def XDayHigh(ticker: str, N: int, condition: Callable[[Any], Any],
              value: float) -> Indicator:
     """Returns the maximum of the high prices over past N days"""
-    def XDayHigh(data_handler: DataHandler) -> float:
-        high: np.ndarray = data_handler.get_latest_symbol_high(ticker, N)
+    def XDayHigh(self, data_handler: DataHandler) -> float:
+        high: np.ndarray = data_handler.get_latest_symbol_high(self.ticker, N)
         max_: float = np.max(high) if len(high) != 0 else np.nan
         logger.debug(high)
         logger.info(max_)
         return max_
 
-    cond = condition
-
-    return condition(XDayHigh, value)
+    return lambda data_handler: operator.lt(XDayHigh(data_handler), value)
