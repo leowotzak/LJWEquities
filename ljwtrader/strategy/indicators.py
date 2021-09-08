a85b958b098c42e2239092998b81062671daed81
Leo Wotzak <leojwotzak@gmail.com>
2021-09-08 13:39:42 -0500

    added indicator wrapper

--- ljwtrader/strategy/indicators.py
+++ ljwtrader/strategy/indicators.py
@@ -5,3 +5,19 @@ from functools import partial
 from ljwtrader.datahandler import DataHandler
 
 logger = logging.getLogger(__name__)
+
+
+def indicator(func: Callable) -> Callable:
+    """Takes the raw array calculation for each indicator and initializes it for the datahandler
+
+    Args:
+        func (Callable): Calculation function for the given indicator
+
+    Returns:
+        Callable: Partially initialized indicator function
+    """
+    def wrapper(ticker: str, N: int, operator: Callable,
+                value: Any) -> Callable:
+        return partial(func, ticker, N, operator, value)
+
+    return wrapper
