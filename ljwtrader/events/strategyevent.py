f9966e5f1437657452585ab0d2d38f951fbd63b0
Leo Wotzak <leojwotzak@gmail.com>
2021-09-07 19:24:45 -0500

    added strategy_id

--- ljwtrader/events/strategyevent.py
+++ ljwtrader/events/strategyevent.py
@@ -6,5 +6,5 @@ from .event import Event
 
 class StrategyEvent(Event):
     """Created in response to price changes in underlying assets"""
-    def __init__(self, ticker: AnyStr, time: datetime):
+    def __init__(self, ticker: AnyStr, time: datetime, strategy_id: str):
         super().__init__('STRATEGY', ticker, time)
