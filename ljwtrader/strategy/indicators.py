d8c17e890c635edaf88b954c1c433e024ccd7f15
Leo Wotzak <leojwotzak@gmail.com>
2021-10-13 11:00:06 -0500

    added ZScore indicator

--- ljwtrader/strategy/indicators.py
+++ ljwtrader/strategy/indicators.py
@@ -33,3 +33,30 @@ def XDayHigh(ticker: str, N: int, operator: Callable, value: Any,
     max_ = np.max(high) if len(high) != 0 else np.nan
     logger.info(f"{ticker}: {max_} {operator} {value}")
     return operator(max_, value)
+
+@indicator
+def ZScore(ticker_one: str,
+           ticker_two: str,
+           N_short: int = None,
+           N_long: int = None,
+           data_handler: DataHandler = None,
+           operator: Callable = None,
+           value: float = None) -> float:
+    """The Z-score utilizing the provided mean and standard deviation lookbacks"""
+    ticker_one_returns = data_handler.get_latest_symbol_adj_close(
+        ticker_one, N_long)
+    ticker_two_returns = data_handler.get_latest_symbol_adj_close(
+        ticker_two, N_long)
+
+    # HACK Cannot divide by zero, probably a cleaner way to do this?
+    if len(ticker_two_returns) == 0:
+        return False
+
+    cointegrated_returns = np.divide(ticker_one_returns, ticker_two_returns)
+
+    short_mean = np.mean(cointegrated_returns[:N_short])
+    long_mean = np.mean(cointegrated_returns)
+    std = np.std(cointegrated_returns)
+    result = (short_mean - long_mean) / std
+    logger.info(f"{ticker_one} -- {ticker_two}: {result} {operator} {value}")
+    return operator(result, value)
