43fc295334ab0ac46f6e5e6ad700c8ff4c80a693
Leo Wotzak <leojwotzak@gmail.com>
2021-10-13 14:10:21 -0500

    changed what happens after backtest runs

--- cli.py
+++ cli.py
@@ -22,9 +22,12 @@ if __name__ == '__main__':
     # TODO Streamline how positions and backtests/streaming are initialized within the system
     # + I believe that each class has its own separate list of tickers, they should 
     # + all reference the same object
+    # + Part of a larger conversation about how to best start the system. One idea is to 
+    # + add all positions to the backtest then pass the backtest to the system
 
     back.add_position_to_backtest(pos1, pos2, pos3, pos4)
     sys.add_position(pos1, pos2, pos3, pos4)
-    res = sys.run_backtest(back)
-
+    sys.run_backtest(back)
+    res = sys._portfolio.generate_historical_portfolio_df()
+    pf.create_returns_tear_sheet(res)
 
