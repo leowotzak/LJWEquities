70ee48824e4ccef1ab0b2b54ee4e3cafbe4475ec
Leo Wotzak <leojwotzak@gmail.com>
2021-10-13 11:01:13 -0500

    added order sizer class placeholders

---
+++ ljwtrader/portfolio/ordersizer.py
@@ -0,0 +1,16 @@
+class FixedDollarValue:
+
+    def __init__(self):
+        pass
+
+
+class PercentPortfolioValue:
+
+    def __init__(self):
+        pass
+
+
+class KelleyCriterion:
+
+    def __init__(self):
+        pass
